096c5c183b91d4f6cd3fa892d665744bb4c5ce24
George Abot <abotabraham18@gmail.com>
2022-01-25 02:01:36 -0500

    stack implementation initial commit

---
+++ stack/MainStackLinkedList.java
@@ -0,0 +1,17 @@
+/**
+ * JAVA APPLICATION CLASS
+ *
+ *@author George Abot Abraham Ret
+ *@version 1.0, 03/09/2021
+ */
+
+class Main {
+  public static void main(String[] args) {
+    Stack newStack = new Stack();
+    newStack.push(1);
+    newStack.push(2);
+    newStack.push(3);
+
+    newStack.deleteStack();
+  }
+}
